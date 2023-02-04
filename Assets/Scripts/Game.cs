cfb7c877fe7189bee698baf373406cb90cc4012c
e205723 <e205723@ie.u-ryukyu.ac.jp>
2023-02-05 02:35:11 +0900

    feat(player): pull grass

--- Assets/Scripts/Game.cs
+++ Assets/Scripts/Game.cs
@@ -4,17 +4,21 @@ using UnityEngine;
 
 public class Game : MonoBehaviour
 {
-    private float grassTime = 0;
-    [SerializeField] float GrassInterval = 0.5f;
-    [SerializeField] GameObject GrassObject;
-    // Start is called before the first frame update
+    private float grassTime = 0; // A variable to track the time since the last grass was placed
+    [SerializeField] float GrassInterval = 0.5f; // The interval between each grass placement
+    [SerializeField] GameObject GrassObject; // The prefab for the grass
+
+    // Function to place a grass object at a random position
     void PlaceGrass()
     {
         Instantiate(GrassObject, new Vector3(Random.Range(-29, 30), 0.5f, Random.Range(-19, 20)), Quaternion.identity);
     }
+
+    // Place grass objects at the start of the game
     void Start()
     {
-        for (int i=1; i <= 40; i++) {
+        for (int i = 1; i <= 40; i++)
+        {
             PlaceGrass();
         }
     }
@@ -22,8 +26,11 @@ public class Game : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // Increase the grassTime by the deltaTime each frame
         grassTime += Time.deltaTime;
-        if (grassTime >= GrassInterval) {
+        // If the grassTime is greater than or equal to the GrassInterval, place a grass object and reset grassTime
+        if (grassTime >= GrassInterval)
+        {
             PlaceGrass();
             grassTime = 0;
         }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,35 +5,76 @@ using System.Collections.Generic;
 
 public class Player : MonoBehaviour
 {
-    float speed = 10.0f;
+    // Number of points the player has earned from pulling grass
+    private int grassPoints = 0;
+    // Speed of the player's movement
+    private float speed = 10.0f;
 
-    [SerializeField] Rigidbody playerRig;
+    [SerializeField] Rigidbody playerRig; // Reference to the player's rigidbody component
+    [SerializeField] PlayerInput m_Input = null; // Reference to the PlayerInput component
 
-    [SerializeField] PlayerInput m_Input = null;
-
+    // Input actions for movement and pulling
     InputAction m_Move;
+    InputAction m_Pull;
 
     private void Start()
     {
+        // Get references to the "Move" and "Pull" actions
         m_Move = m_Input.currentActionMap.FindAction("Move");
+        m_Pull = m_Input.currentActionMap.FindAction("Pull");
     }
 
     private void Update()
     {
+        // Call the Move method each frame
         Move();
+
+        // Subscribe to the performed event for the "Pull" action
+        m_Pull.performed += context => buttonActionB();
     }
 
+    // Method for moving the player
     void Move()
     {
         Vector2 vector = m_Move.ReadValue<Vector2>() * speed;
 
+        // If the input vector is not equal to zero
         if (vector.sqrMagnitude > 0)
         {
+            // Update the player's velocity and forward direction
             playerRig.velocity = new Vector3(vector.x, 0, vector.y);
+            transform.forward = new Vector3(vector.x, 0, vector.y);
         }
         else
         {
+            // If the input vector is equal to zero, set the player's velocity to zero
             playerRig.velocity = Vector3.zero;
+        }
+    }
+
+    // Method for pulling grass
+    void PullGrass(Grass grass)
+    {
+        // Increase the player's grass points and destroy the pulled grass object
+        grassPoints += grass.GetPoints();
+        Destroy(grass.gameObject);
+        Debug.Log(grassPoints);
+    }
+
+    // Method called when the "Pull" action is performed
+    void buttonActionB()
+    {
+        RaycastHit raycastHitObject;
+        // If a grass object is within the range of the raycast
+        if (Physics.Raycast(gameObject.transform.position, transform.forward, out raycastHitObject, 2f))
+        {
+            Grass grass = raycastHitObject.transform.GetComponent<Grass>();
+            // If the raycast hit object has a Grass component
+            if (grass != null)
+            {
+                // Call the PullGrass method
+                PullGrass(grass);
+            }
         }
     }
 }
