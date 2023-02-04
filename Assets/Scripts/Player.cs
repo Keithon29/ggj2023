82cb92078773ab2e2d6bd5833c29990f9ad73681
e205723 <e205723@ie.u-ryukyu.ac.jp>
2023-02-05 05:08:30 +0900

    feat(player): plant tree

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,74 +5,107 @@ using System.Collections.Generic;
 
 public class Player : MonoBehaviour
 {
-    // Number of points the player has earned from pulling grass
+    // Private integer variable to keep track of grass points collected by the player
     private int grassPoints = 0;
-    // Speed of the player's movement
+
+    // Private list to store the trees the player has planted
+    private List<GameObject> trees = new List<GameObject>();
+
+    // Private float to store the speed of the player
     private float speed = 10.0f;
 
-    [SerializeField] Rigidbody playerRig; // Reference to the player's rigidbody component
-    [SerializeField] PlayerInput m_Input = null; // Reference to the PlayerInput component
+    // Reference to the tree object prefab
+    [SerializeField] GameObject treeObject;
 
-    // Input actions for movement and pulling
+    // Reference to the Rigidbody component of the player
+    [SerializeField] Rigidbody playerRig;
+
+    // Reference to the PlayerInput component for input management
+    [SerializeField] PlayerInput m_Input = null;
+
+    // Variables to store the input actions for moving, planting, and pulling
     InputAction m_Move;
+    InputAction m_Plant;
     InputAction m_Pull;
 
     private void Start()
     {
-        // Get references to the "Move" and "Pull" actions
+        // Find the actions "Move", "Plant", and "Pull" in the current action map
         m_Move = m_Input.currentActionMap.FindAction("Move");
+        m_Plant = m_Input.currentActionMap.FindAction("Plant");
         m_Pull = m_Input.currentActionMap.FindAction("Pull");
     }
 
     private void Update()
     {
-        // Call the Move method each frame
+        // Call the Move function
         Move();
 
-        // Subscribe to the performed event for the "Pull" action
+        // Add the buttonActionA method as a delegate to the performed event of the Plant action
+        m_Plant.performed += context => buttonActionA();
+
+        // Add the buttonActionB method as a delegate to the performed event of the Pull action
         m_Pull.performed += context => buttonActionB();
     }
 
-    // Method for moving the player
+    // Function to handle player movement
     void Move()
     {
+        // Read the value of the move action and multiply it by the speed
         Vector2 vector = m_Move.ReadValue<Vector2>() * speed;
 
-        // If the input vector is not equal to zero
+        // If the magnitude of the vector is greater than 0, move the player and face the forward direction
         if (vector.sqrMagnitude > 0)
         {
-            // Update the player's velocity and forward direction
             playerRig.velocity = new Vector3(vector.x, 0, vector.y);
             transform.forward = new Vector3(vector.x, 0, vector.y);
         }
+        // If the magnitude of the vector is 0, stop the player
         else
         {
-            // If the input vector is equal to zero, set the player's velocity to zero
             playerRig.velocity = Vector3.zero;
         }
     }
 
-    // Method for pulling grass
-    void PullGrass(Grass grass)
+    // Function to plant a tree
+    void plantTree()
     {
-        // Increase the player's grass points and destroy the pulled grass object
-        grassPoints += grass.GetPoints();
-        Destroy(grass.gameObject);
-        Debug.Log(grassPoints);
+        // Instantiate a tree object in front of the player and add it to the trees list
+        trees.Add(Instantiate(treeObject, transform.position + transform.forward, Quaternion.identity));
     }
 
-    // Method called when the "Pull" action is performed
+    // Function to handle the action performed when the A button is pressed
+    void buttonActionA()
+    {
+        // Check if there is no obstacle in front of the player
+        if (!Physics.Raycast(gameObject.transform.position, transform.forward, 2f))
+        {
+            // Call the plantTree function
+            plantTree();
+        }
+    }
+
+    // Function to handle the action of pulling grass
+    private void PullGrass(Grass grass)
+    {
+        // Increase the grass points when pulling the grass
+        grassPoints += grass.GetPoints();
+        // Destroying the grass game object
+        Destroy(grass.gameObject);
+    }
+
+    // Function to handle the action performed when the B button is pressed
     void buttonActionB()
     {
         RaycastHit raycastHitObject;
-        // If a grass object is within the range of the raycast
+        // Raycast to check if the player is facing any object with 2 units distance
         if (Physics.Raycast(gameObject.transform.position, transform.forward, out raycastHitObject, 2f))
         {
+            // Check if the object has a Grass component
             Grass grass = raycastHitObject.transform.GetComponent<Grass>();
-            // If the raycast hit object has a Grass component
             if (grass != null)
             {
-                // Call the PullGrass method
+                // Call the PullGrass function to handle pulling the grass
                 PullGrass(grass);
             }
         }
